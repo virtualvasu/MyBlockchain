98ab723a41b1787b475a589690fb74eba787e9c6
virtualvasu <virtualvasu624@gmail.com>
2024-12-15 22:21:40 +0530

    chain update

---
+++ blockchain/Chain.js
@@ -0,0 +1,46 @@
+const Block = require('./Block');
+
+class Chain {
+    createGenesisBlock() {
+        return new Block(0, Date.now(), "This is a genesis block", "0");
+    }
+
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.prevHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calcHash();
+        this.chain.push(newBlock);
+    }
+
+    isValidChain() {
+
+        if (JSON.stringify(this.chain[0]) !== JSON.stringify(this.createGenesisBlock())) {
+            return false;
+        }
+
+
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const prevBlock = this.chain[i - 1];
+
+            if (currentBlock.prevHash !== prevBlock.hash) {
+                return false;
+            }
+
+            if (currentBlock.hash !== currentBlock.calcHash()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports = Chain;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express')
+const express = require('express');
+const crypto = require("crypto-js");
+const Web3 = require('web3');
 const app = express()
 const port = 3000
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World everyone!')
